545dcfd2c6e9af4d361d57d1ce2ea325469e02ad
Krystyna Rud <kristina.rud5@gmail.com>
2023-07-27 16:00:33 +0300

    added icons

---
+++ task1/js/categories.js
@@ -0,0 +1,10 @@
+export const categories = [
+    { id: 0, title: "Task", icon: "./images/task.png" },
+    { id: 1, title: "Random Thought", icon: "./images/thinking.png" },
+    { id: 2, title: "Idea", icon: "./images/idea.png" },
+];
+
+export function getCategory(category) {
+    const index = categories.find(c => c.id === category);
+    return index;
+};
--- task1/js/index.js
+++ task1/js/index.js
@@ -1,24 +1,20 @@
 import { btnCreateNotes, btnCloseModal, table } from "./constants.js";
-import { sendRequest } from "./api.js";
-import { API_URL } from "./constants.js";
+import {  notes } from "./notes.js";
+import { getCategory } from './categories.js'
 
-const data = [];
 document.addEventListener("DOMContentLoaded", () => {
-  sendRequest(API_URL).then((res) => {
-    data.push(...res);
-    table.append(...renderData(data));
-  });
-
   btnCreateNotes.addEventListener("click", openModal);
   btnCloseModal.addEventListener("click", closeModal);
+  table.append(...renderData(notes));
 });
 
 const renderRow = ({ name, created, category, content, dates }, id) => {
   const tr = document.createElement("tr");
+  const {id:idCategory, title, icon} = getCategory(category);
   tr.id = id;
-  tr.innerHTML = `<td id="name">${name}</td>
+  tr.innerHTML = `<td id="name"> <img src=${icon} alt=${title} class="item-icon" /> ${name}</td>
                   <td id="created">${created}</td>
-                  <td id="category">${category}</td>
+                  <td id="category" data-category="${idCategory}">${title}</td>
                   <td id="content">${content}</td>
                   <td id="dates">${dates}</td>
                   <td id="edit">Edit</td>
@@ -26,21 +22,23 @@ const renderRow = ({ name, created, category, content, dates }, id) => {
                   <td id="delete">Delete</td>`;
   return tr;
 };
-const renderData = (data) => {
-  const arrayRows = data?.map((item, index) => renderRow(item, index));
+
+
+const renderData = (notes) => {
+  const arrayRows = notes?.map((item, index) => renderRow(item, index));
   return arrayRows;
 };
 
 // Функция для открытия модального окна
 function openModal() {
-  var modal = document.getElementById("modal");
+  const modal = document.getElementById("modal");
   modal.style.display = "block";
   document.body.style.overflow = "hidden";
 }
 
 // Функция для закрытия модального окна
 function closeModal() {
-  var modal = document.getElementById("modal");
+  const modal = document.getElementById("modal");
   modal.style.display = "none";
   document.body.style.overflow = "auto";
 }
